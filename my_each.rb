7488389c2a9841e037ebc29e9975224194311dc8
Madelyn Deshield <madelynwallop@gmail.com>
2019-11-10 14:01:18 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
-  # code here
+#require 'pry'
+def my_each(collection)
+  i = 0 
+  while i < 
+  i = i + 1 
+   #binding.pry
+    yield 
+    puts i 
+  end 
+end 
 end
+  
+  # code here
+#end
